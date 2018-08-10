ba9f6b14c1e7defa71d17e4931f9ba464f27f010
Dieter Menne <dieter.menne@menne-biomed.de>
2018-08-10 10:09:49 +0200

    Made port for run_shiny a parameter

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -1,11 +1,11 @@
 #' Run breathtestshiny app
-#'
+#' @param port Port for runApp, default 3838
 #' @export
 #' @importFrom shiny runApp
-run_shiny = function() {
+run_shiny = function(port = 3838) {
   appDir = system.file("shiny", package = "breathtestshiny")
   if (appDir == "") {
     stop("Could not Shiny app in breathtestshiny", call. = FALSE)
   }
-  runApp(appDir, display.mode = "normal")
+  runApp(appDir, port = port, display.mode = "normal")
 }
