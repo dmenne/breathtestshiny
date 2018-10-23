4f17b9eb653bd4ab92812e5be6eaa0a70a7389cd
Dieter Menne <dieter.menne@menne-biomed.de>
2018-10-23 11:51:26 +0200

    Activated tests

--- tests/testthat/test-app-file.R
+++ tests/testthat/test-app-file.R
@@ -4,7 +4,7 @@ library(shinytest)
 #library(V8) # required on travis, but does not work there because of different results
 # in lower decimals
 
-#test_that("default_demo_test works", {
-#  appdir <- system.file(package = "breathtestshiny", "shiny")
-#  expect_pass(testApp(appdir, compareImages = FALSE, quiet = FALSE))
-#})
+test_that("default_demo_test works", {
+  appdir <- system.file(package = "breathtestshiny", "shiny")
+  expect_pass(testApp(appdir, compareImages = FALSE, quiet = FALSE))
+})
