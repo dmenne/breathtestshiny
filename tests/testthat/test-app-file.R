1765dfc86e365d9a517a2ef30e116f13eb518d36
Dieter Menne <dieter.menne@menne-biomed.de>
2018-10-23 19:06:16 +0200

    Including suggests again

--- tests/testthat/test-app-file.R
+++ tests/testthat/test-app-file.R
@@ -1,7 +1,7 @@
 context("breathtestshiny-app")
 # This file is for testing the applications in the inst/ directory.
 library(shinytest)
-#library(V8) # required on travis, but does not work there because of different results
+library(V8) # required on travis, but does not work there because of different results
 # in lower decimals
 
 test_that("default_demo_test works", {
