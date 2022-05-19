6384db8cf1c5dd71250fd683d82dcd184ff30595
Dieter Menne <dieter.menne@menne-biomed.de>
2022-05-19 15:52:55 +0200

    Removed redundant javascript

--- inst/shiny/www/message-handler.js
+++ inst/shiny/www/message-handler.js
@@ -1,24 +1,3 @@
-Shiny.addCustomMessageHandler("replace_url",
-  function(url) {
-    location.replace(url);
-  }
-);
-
-(function($) {
-    $.QueryString = (function(a) {
-        if (a === "") return {};
-        var b = {};
-        for (var i = 0; i < a.length; ++i)
-        {
-            var p=a[i].split('=', 2);
-            if (p.length != 2) continue;
-            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-        }
-        return b;
-    })(window.location.search.substr(1).split('&'));
-})(jQuery);
-
-
 shinyBS.addTooltip = function(id, type, opts) {
   var $id = shinyBS.getTooltipTarget(id);
   var dopts = {html: true};
