dc998f10f7e9ce7ce325205b2421a71281c2d5b7
Dieter Menne <dieter.menne@menne-biomed.de>
2024-11-22 17:46:00 +0100

    Using _PACKAGE; version 0.5.3

--- R/breathtestshiny.R
+++ R/breathtestshiny.R
@@ -1,7 +1,9 @@
 #' @title breathtestshiny
 #'
 #' @description Web app to read and analyze 13C breath test data for gastric emptying
-#' @docType package
+#' @keywords internal
+#' "_PACKAGE"
+#' @name breathtestshiny-package
 #' @import breathtestcore
 #' @import breathteststan
 
